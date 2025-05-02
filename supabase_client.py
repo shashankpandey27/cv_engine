d6dbea0f204c8c0845a54c49e93a42ca1efee6aa
shashankpandey27 <141110838+shashankpandey27@users.noreply.github.com>
2025-05-02 18:53:27 +0400

    Update supabase_client.py

--- supabase_client.py
+++ supabase_client.py
@@ -3,8 +3,8 @@ import streamlit as st
 
 # --- Supabase Config ---
 # Access secrets securely
-SUPABASE_URL = st.secrets["supabase"]["url"]
-SUPABASE_KEY = st.secrets["supabase"]["key"]
-SUPABASE_SERVICE_KEY = st.secrets["supabase"]["service_key"]
-BUCKET_NAME = st.secrets["supabase"]["bucket"]
+SUPABASE_URL = st.secrets["supabase_engine"]["url"]
+SUPABASE_KEY = st.secrets["supabase_engine"]["key"]
+SUPABASE_SERVICE_KEY = st.secrets["supabase_engine"]["service_key"]
+BUCKET_NAME = st.secrets["supabase_engine"]["bucket"]
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_SERVICE_KEY)
