927671fe7186fe878fecc694ddf6ff715a70f710
shashankpandey27 <141110838+shashankpandey27@users.noreply.github.com>
2025-05-03 19:31:58 +0400

    Update 1_Resume_Analyser.py

--- pages/1_Resume_Analyser.py
+++ pages/1_Resume_Analyser.py
@@ -30,7 +30,7 @@ if st.session_state.get("authentication_status") != True:
     st.warning("Please login to access this page.")
     st.stop()
 
-
+st.set_page_config(layout="center")
 
 # Inject logo and favicon
 def add_favicon():
