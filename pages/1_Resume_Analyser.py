a645f876aecd023d9257c225d6c0e5ebaafdfc0b
shashankpandey27 <141110838+shashankpandey27@users.noreply.github.com>
2025-05-06 20:54:11 +0400

    Update 1_Resume_Analyser.py

--- pages/1_Resume_Analyser.py
+++ pages/1_Resume_Analyser.py
@@ -439,7 +439,8 @@ def generate_role_scores_and_upload(uploaded_cv):
             st.warning("This CV already exists.")
         else:
             # Upload file
-            supabase.storage.from_(BUCKET_NAME).upload(public_path, uploaded_cv.read(), {"content-type": "application/pdf"})
+            supabase.storage.from_(BUCKET_NAME).upload(public_path, uploaded_cv.read(),
+                                                       file_options = {"Content-Type": "application/pdf", "upsert": "true"})
  
             # Get public URL
             url = supabase.storage.from_(BUCKET_NAME).get_public_url(public_path)
